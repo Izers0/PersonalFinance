e33516daa6e36a456c1f6ebb7ed1436ab0c058f2
Isaac <isaacatkinson04@gmail.com>
2025-01-13 03:42:48 +0000

    Transaction Function removed

--- .venv/userData.py
+++ .venv/userData.py
@@ -1,14 +1,7 @@
-# transaction function
+# Required Information
+transactionDate = input("Enter the date: ") # later make this a date selector box thing
+transactionAmount = int(input("How much did you spend: £"))
+transactionCategory = input("What type of transaction was this: ") # later make this a menu
 
-def transaction():
-
-    # Required Information
-    date = input("Enter the date: ") # later make this a date selector box thing
-    amount = input(int("How much did you spend: £"))
-    category = input("What type of transaction was this: ") # later make this a menu
-
-    # Optional Information
-    description = input("Enter a description for the purchase e.g why you bought it: ")
-
-
-transaction()
+# Optional Information
+transactionDescription = input("Enter a description for the purchase e.g why you bought it: ")
