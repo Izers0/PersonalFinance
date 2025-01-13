c7f5ca101b922ccb2a95a55c311e2c66706c51f9
Isaac <isaacatkinson04@gmail.com>
2025-01-13 03:35:29 +0000

    Transaction Function

--- .venv/userData.py
+++ .venv/userData.py
@@ -1,4 +1,14 @@
-print("Yes now")
-print("banana")
-print("Blue")
-print(123)
+# transaction function
+
+def transaction():
+
+    # Required Information
+    date = input("Enter the date: ") # later make this a date selector box thing
+    amount = input(int("How much did you spend: £"))
+    category = input("What type of transaction was this: ") # later make this a menu
+
+    # Optional Information
+    description = input("Enter a description for the purchase e.g why you bought it: ")
+
+
+transaction()
