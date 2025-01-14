47ee404b7c748b7c094fbd88a40352756d9b50b9
Isaac <isaacatkinson04@gmail.com>
2025-01-14 04:33:44 +0000

    Added validation for transactionAmount
    Changed the list of dictionaries to a list
    
    Made a loop to iterate over each item in the list
    
    Used datetime to import date to automatically get the date of the transaction for the user

--- Transactions.py
+++ Transactions.py
@@ -1,14 +1,32 @@
+from datetime import date
+
+# validation of transactionAmount
+def validAmount(amount):
+    return amount > 0
+
+
 # Required Information
-transactionDate = input("Enter the date: ") # later make this a date selector box thing
+# This will return today's date for the user. This will allow us to track their spending
+today = date.today()
+
+transactionDate = today.strftime("Today is the %d/%m/%Y") # dd/mm/YY
 transactionAmount = int(input("How much did you spend: £"))
 transactionCategory = input("What type of transaction was this: ") # later make this a menu
 
 # Optional Information
 transactionDescription = input("Enter a description for the purchase e.g why you bought it: ")
 
-transactions = [{transactionDate},
-                {transactionAmount},
-                {transactionCategory},
-                {transactionDescription}]
+
+# Lists all the transaction information
+transactions = (transactionDate, transactionAmount, transactionCategory, transactionDescription)
 
 
+# check to see the transactionAmount is above 0
+if validAmount(transactionAmount):
+    for i in transactions:
+        print(i)
+
+    print(f"You spent £{transactionAmount}. This has been added to the tracker")
+
+else:
+    print("This is an invalid value. Please enter a positive amount")
